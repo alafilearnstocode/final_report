66d60b8e77b7a326e99de7e987cb5412518cf6b5
alafilearnstocode <emmanuelalafi2004@gmail.com>
2025-05-24 15:25:10 -0500

    setup

---
+++ final_report/docs/appendices/user_feedback_summary.md
@@ -0,0 +1,5 @@
+---
+sidebar_position: 6
+---
+
+# User Feedback and Testing Summary
---
+++ final_report/docusaurus.config.ts
@@ -0,0 +1,104 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Style Sync',
+  tagline: 'We all love to dress for the weather.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://therecyclery.vercel.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            to: '/docs/intro',
+            from: ['/'],
+          },
+        ],
+      },
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Style Sync',
+      logo: {
+        alt: 'Style Sync Logo',
+        src: 'img/logo.svg',
+        href: 'https://therecyclery.vercel.app', // link to our site
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/alafilearnstocode/final_report.git',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: 'Final Report',
+              to: '/docs/intro',
+            },
+          ],
+        },
+    
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} DTCTeam22.1 Northwestern University Segal Design Institute`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
